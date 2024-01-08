1696dc3c192488d55913c991f25955618ef9e5d9
SyerliNP <serlynindi680@gmail.com>
2024-01-08 15:30:36 +0700

    kelolaowner

--- resources/views/admin/kelolaowner.blade.php
+++ resources/views/admin/kelolaowner.blade.php
@@ -4,7 +4,84 @@
 
 @section('isi')
 <div class="sm:ml-64">
-    <p>ertyuio</p>
-    <h2>ertyui</h2>
+<h1 class="mb-4 mt-4 ml-4  text-3xl font-black text-gray-900 dark:text-white">Kelola Owner</h1>
 </div>
+<div class="sm:ml-64 grid grid-cols-3 gap-4">
+<div class="bg-gray-200 p-4 text-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('foto/login5.jpg') }}" alt="Bonnie image"/>
+        <br>
+        <h5 class="mb-1 text-2xl font-semibold text-gray-900 dark:text-white">Nautilius Sarvabek</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">nautiliussar@gmail.com</span>
+        <div class="flex mt-4 md:mt-6">
+            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Hapus</a>
+            {{-- <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a> --}}
+        </div>
+    </div>
+</div>
+<div class="bg-gray-200 p-4 text-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('foto/login5.jpg') }}" alt="Bonnie image"/>
+        <br>
+        <h5 class="mb-1 text-2xl font-semibold text-gray-900 dark:text-white">Nautilius Sarvabek</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">nautiliussar@gmail.com</span>
+        <div class="flex mt-4 md:mt-6">
+            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Hapus</a>
+            {{-- <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a> --}}
+        </div>
+    </div>
+</div>
+<div class="bg-gray-200 p-4 text-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('foto/login5.jpg') }}" alt="Bonnie image"/>
+        <br>
+        <h5 class="mb-1 text-2xl font-semibold text-gray-900 dark:text-white">Nautilius Sarvabek</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">nautiliussar@gmail.com</span>
+        <div class="flex mt-4 md:mt-6">
+            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Hapus</a>
+            {{-- <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a> --}}
+        </div>
+    </div>
+</div>
+
+<div class="bg-gray-200 p-4 text-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('foto/login5.jpg') }}" alt="Bonnie image"/>
+        <br>
+        <h5 class="mb-1 text-2xl font-semibold text-gray-900 dark:text-white">Nautilius Sarvabek</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">nautiliussar@gmail.com</span>
+        <div class="flex mt-4 md:mt-6">
+            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Hapus</a>
+            {{-- <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a> --}}
+        </div>
+    </div>
+</div>
+
+<div class="bg-gray-200 p-4 text-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('foto/login5.jpg') }}" alt="Bonnie image"/>
+        <br>
+        <h5 class="mb-1 text-2xl font-semibold text-gray-900 dark:text-white">Nautilius Sarvabek</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">nautiliussar@gmail.com</span>
+        <div class="flex mt-4 md:mt-6">
+            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Hapus</a>
+            {{-- <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a> --}}
+        </div>
+    </div>
+</div>
+
+<div class="bg-gray-200 p-4 text-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('foto/login5.jpg') }}" alt="Bonnie image"/>
+        <br>
+        <h5 class="mb-1 text-2xl font-semibold text-gray-900 dark:text-white">Nautilius Sarvabek</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400">nautiliussar@gmail.com</span>
+        <div class="flex mt-4 md:mt-6">
+            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Hapus</a>
+            {{-- <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a> --}}
+        </div>
+    </div>
+</div>
+</div>
+<br><br><br>
 @endsection
--- resources/views/layout/sidebar_user.blade.php
+++ resources/views/layout/sidebar_user.blade.php
@@ -45,8 +45,8 @@
                 </div>
             </li>
             <li>
-                <a href="/"
-                    class="flex items-center p-2 text-gray-900 rounded-lg {{ request()->is('/') ? 'bg-lime-700 text-white' : 'bg-gray-200' }}">
+                <a href="/dahsboard"
+                    class="flex items-center p-2 text-gray-900 rounded-lg {{ request()->is('/dashboard') ? 'bg-lime-700 text-white' : 'bg-gray-200' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path
                             d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
@@ -57,8 +57,8 @@
                 </a>
             </li>
             <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg {{ request()->is('') ? 'bg-lime-700 text-white' : 'bg-gray-200' }}">
+                <a href="/kamarkami"
+                    class="flex items-center p-2 text-gray-900 rounded-lg {{ request()->is('/kamarkami') ? 'bg-lime-700 text-white' : 'bg-gray-200' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path fill-rule="evenodd"
                             d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z"
