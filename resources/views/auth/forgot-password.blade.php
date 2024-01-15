3ffe262dcdec8e0e4733c51579ac00279911629a
SyerliNP <serlynindi680@gmail.com>
2024-01-15 14:59:59 +0700

    profile

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,45 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+@extends('layout.mainauth')
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('isi')
+  <div class="relative flex justify-center items-center h-screen">
+    <div class="absolute inset-0 bg-[url('foto/background.jpg')] bg-cover bg-center brightness-50"></div>
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <a href="#"
+      class="relative block max-w-xl max-h-100 p-6 bg-[#739072] border border-gray-200 rounded-lg shadow text-center">
+      <form method="POST" action="{{ route('password.email') }}">
         @csrf
+        <div class="justify-center">
+          <div class="flex items-center justify-center mb-1">
+            <img src="{{ asset('foto/inilogo.png') }}" alt="" width="150" class="mb-2">
+          </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+          <h3 class="mb-6 text-3xl font-xl text-gray-50 ">Lupa Password</h3>
+          <p class="text-white mb-6 text-sm mr-10 ml-10">Lupa kata sandi Anda? Tidak masalah. Cukup beri tahu kami alamat
+            email Anda dan kami akan mengirimkan tautan pengaturan ulang kata sandi melalui email yang memungkinkan Anda
+            memilih kata sandi baru.</p>
+        </div>
+        {{-- <label for="email-address-icon" class="block mb-2 text-sm font-medium text-gray-50 ">E-mail</label> --}}
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+          </div>
+          <input type="text" id="email-address-icon"
+            class="mx-auto mb-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-10/12  "
+            placeholder="emailmu@gmail.com">
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+
+        <div class="flex justify-between mt-4 mr-10 ml-10">
+          <button type="button"
+            class="text-dark bg-[#D2E3C8] hover:bg-[#A0D084] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            {{ __('Kembali') }}
+          </button>
+
+          <button type="button"
+            class="text-white bg-[#38543B] hover:bg-[#A0D084] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            {{ __('Send to Mail') }}
+          </button>
         </div>
-    </form>
-</x-guest-layout>
+      </form>
+    </a>
+  </div>
+@endsection
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,38 +1,102 @@
 @extends('layout.main')
 
-@if ( Auth::user()->role == 'user')
-    @extends('layout.sidebar_user')
-@elseif ( Auth::user()->role == 'owner')
-    @extends('layout.sidebar_owner')
+@if (Auth::user()->role == 'user')
+  @extends('layout.sidebar_user')
+@elseif (Auth::user()->role == 'owner')
+  @extends('layout.sidebar_owner')
 @else
-    @extends('layout.sidebar_admin')
+  @extends('layout.sidebar_admin')
 @endif
 
 @section('isi')
-    <div class="sm:ml-64 bg-[#E8F1E3]">
-        <p class="font-semibold text-xl py-10 text-center text-gray-800 leading-tight">
-            Profil Akun
-        </p>
-        <div class="py-6">
-            <div class="grid grid-cols-2 gap-3 mx-auto sm:px-6 lg:px-8">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-profile-information-form')
+  <div class="sm:ml-64 bg-[#E8F1E3]">
+    <div class="grid grid-cols-3 gap-5 ">
+                <div class="bg-white p-3 rounded-lg shadow-md mt-6 ml-4">
+                    <!-- Profile Image -->
+                    <div class="p-3 relative w-40 h-40 mx-auto border-4 border-white rounded-full overflow-hidden ">
+                        <img src="{{ asset('foto/dummy.jpeg') }}" alt="Profile" class="w-full rounded-full object-cover">
+                    </div>
+
+                    <!-- User Information -->
+                    <div class="p-3 mt-0 text-center"> <!-- Adjusted margin-top to mt-6 -->
+                        <h2 class="text-2xl font-semibold text-gray-800">Nama Pengguna</h2>
+                        <p class="text-gray-600">user@gmail.com</p>
+                    </div>
+                    <div class="p-3 mt-0 text-center"> <!-- Adjusted margin-top to mt-6 -->
+                        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Ganti</button>
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Upload</button>
                     </div>
                 </div>
-    
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-password-form')
-                    </div>
-                </div>
-    
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg col-span-2">
-                    <div class="max-w-xl">
-                        @include('profile.partials.delete-user-form')
-                    </div>
-                </div>
-            </div>
+
+      <div class=" col-span-2 bg-white rounded-md mt-6 px-[30px] py-[30px] shadow-md mr-4">
+        <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+          @csrf
+          <div class="mb-2 mt-6">
+            <div class="grid grid-cols-3">
+            <label for="nama" class="block mb-2 text-sm font-medium text-gray-900">Nama</label>
+            <input type="nama" id="nama"
+              class="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="nama mu" required>
+          </div>
         </div>
+          <div class="mb-2 mt-6">
+            <div class="grid grid-cols-3">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+            <input type="email" id="email"
+              class="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="emailmu@gmail.com" required>
+          </div>
+          </div>
+          <div class="mb-2 mt-6">
+            <div class="grid grid-cols-3">
+            <label for="number" class="block mb-2 text-sm font-medium text-gray-900">Nomor
+              Handphone</label>
+            <input type="number" id="number"
+              class="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="08*********" required>
+            </div>
+          </div>
+          <div class="flex justify-end">
+            <button type="submit"class="mt-4  ml-2 mr-2 text-white bg-blue-600 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Simpan</button>
+          </div>
+        </form>
+      </div>
     </div>
+    <div class="p-4 col-span-2 bg-white rounded-md  mt-4 shadow-md mr-4 ml-4">
+        <h5 class="text-lg font-semibold text-gray-800 mb-4 text-center mb-5">Ubah Password</h5>
+        <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+          @csrf
+          <div class="mb-2 mt-4">
+            <div class="grid grid-cols-3">
+                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900">Password Lama</label>
+                <input type="nama" id="nama"
+                  class="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                  placeholder="nama mu" required>
+            </div>
+          </div>
+          <div class="mb-2 mt-4">
+            <div class="grid grid-cols-3">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Password Baru</label>
+            <input type="email" id="email"
+              class="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="emailmu@gmail.com" required>
+          </div>
+        </div>
+          <div class="mb-2 mt-4">
+            <div class="grid grid-cols-3">
+            <label for="number" class="block mb-2 text-sm font-medium text-gray-900">Konfirmasi Password</label>
+            <input type="number" id="number"
+              class="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="08*********" required>
+            </div>
+          </div>
+          <div class="flex justify-end">
+            <button type="submit"class="mt-4  ml-2 mr-2 text-white bg-blue-600 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Simpan</button>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
+
+  </div>
 @endsection
