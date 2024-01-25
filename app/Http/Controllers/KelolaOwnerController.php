8cba448fa0c3f29eabc8c9df03effbbddd0d181a
rendi <rendydwisaputra250@gmail.com>
2024-01-25 15:25:02 +0700

    kelola owner

--- app/Http/Controllers/KelolaOwnerController.php
+++ app/Http/Controllers/KelolaOwnerController.php
@@ -2,40 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Kamar;
+use Illuminate\Http\Request;
 
 class KelolaOwnerController extends Controller
 {
     public function index()
     {
         $owners = auth()->user();
+
         $owners = User::where('id', '!=',  $owners->id)
             ->where('role',  'owner')
             ->get();
         return view('admin.kelolaowner', compact('owners'));
     }
 
-   
+
 
     public function show(User $owner)
     {
         $owner = User::find($owner);
 
-        if(!$owner){
-            return redirect()->route('owner.show')->with('error','User tidak ditemukan');
+        if (!$owner) {
+            return redirect()->route('owner.show')->with('error', 'User tidak ditemukan');
         }
         return view('owner.show', ['user' => $owner]);
     }
     public function delete(User $owner)
-{
-    // Add validation or authorization checks as needed
+    {
+        // Add validation or authorization checks as needed
 
-    // Perform the delete action
-    $owner->delete();
+        // Perform the delete action
+        $owner->delete();
 
-    // Redirect back to the owner list with a success message
-    return redirect()->route('kelola-admin')->with('success', 'Owner deleted successfully');
-}
-
+        // Redirect back to the owner list with a success message
+        return redirect()->route('kelola-admin')->with('success', 'Owner deleted successfully');
+    }
 }
--- resources/views/admin/kelolaowner.blade.php
+++ resources/views/admin/kelolaowner.blade.php
@@ -16,17 +16,18 @@
                         <div class="flex mt-4 md:mt-6 gap-2">
                             <a href="{{ route('kelolaowner.delete', ['owner' => $owner->id]) }}"
                                 class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 ">Hapus</a>
-                                <a href="#" data-modal-target="popup-modal{{ $owner->id }}"
-                                    data-modal-toggle="popup-modal{{ $owner->id }}"
-                                    class="inline-flex items-center font-medium text-center text-white bg-[#4F6F52] focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-3 py-1.5 me-2 mb-2"
-                                    role="button">
-                                     Info
-                                 </a>
+                            <a href="#" data-modal-target="popup-modal{{ $owner->id }}"
+                                data-modal-toggle="popup-modal{{ $owner->id }}"
+                                class="inline-flex items-center font-medium text-center text-white bg-[#4F6F52] focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-3 py-1.5 me-2 mb-2"
+                                role="button">
+                                Info
+                            </a>
 
                         </div>
                         <div id="popup-modal{{ $owner->id }}" tabindex="-1"
                             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50  items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                             <div class="relative p-4 w-full max-w-md max-h-full">
+                                <input type="hidden" name="owner" value="{{ $owner->id }}">
                                 <div class="relative bg-white rounded-lg shadow py-6">
                                     <button type="button"
                                         class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
@@ -54,15 +55,25 @@
                                                 class="bg-[#4F6F52] p-2 w-10 h-10 me-4 flex items-center justify-center text-lg rounded">
                                                 <i class="fa-solid fa-house text-white"></i>
                                             </div>
-                                            <span class="font-semibold text-lg text-gray-900 truncate">Memiliki 2 Tempat
+                                            <span class="font-semibold text-lg text-gray-900 truncate">Memiliki
+                                                {{ $owner->kos->count() }} Tempat
                                                 Kost</span>
                                         </div>
                                         <div class="kost flex items-center">
+                                            @php
+                                                $totalKamarCount = 0;
+                                            @endphp
+                                            @foreach ($owner->kos as $kos)
+                                                @php
+                                                    $totalKamarCount += $kos->kamar->count();
+                                                @endphp
+                                            @endforeach
                                             <div
                                                 class="bg-[#4F6F52] p-2 w-10 h-10 me-4 flex items-center justify-center text-lg rounded">
                                                 <i class="fa-solid fa-house text-white"></i>
                                             </div>
-                                            <span class="font-semibold text-lg text-gray-900 truncate">Memiliki 125
+                                            <span class="font-semibold text-lg text-gray-900 truncate">Memiliki
+                                                {{ $totalKamarCount }}
                                                 Kamar</span>
                                         </div>
                                     </div>
