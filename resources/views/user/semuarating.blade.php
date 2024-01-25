fc36e963d44037f77b54a2cc1c77a6c2f5d75687
SyerliNP <serlynindi680@gmail.com>
2024-01-25 09:03:19 +0700

    semua rating

--- resources/views/user/semuarating.blade.php
+++ resources/views/user/semuarating.blade.php
@@ -3,20 +3,22 @@
 @extends('layout.sidenavbar')
 
 @section('isi')
-    <div class="py-10 px-[140px] px-[20px] mt-20">
+    <div class="py-10 px-[90px] px-[90px] mt-20">
         @foreach ($ulasans as $ulasan)
-            <div class="py-3 px-5 rounded-lg mb-5">
+            <div class="py-3 px-15 rounded-lg mb-5">
                 <div class="flex gap-4">
                     <div class="profile">
-                        <img src="{{ asset('profiles/' . $ulasan->user->foto) }}" alt="" width="100" class="rounded-full">
+                        <img src="{{ asset('profiles/' . $ulasan->user->foto) }}" alt="" width="50" class="rounded-full">
                     </div>
                     <div class="Informasinya flex-grow">
                         <div class="flex justify-between">
                             <div>
                                 <h1 class="text-2xl font-semibold text-gray-900">{{ $ulasan->user->name }}</h1>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10s10-4.477 10-10M12 6.25a.75.75 0 0 1 .75.75v6a.75.75 0 0 1-1.5 0V7a.75.75 0 0 1 .75-.75M12 17a1 1 0 1 0 0-2a1 1 0 0 0 0 2" clip-rule="evenodd"/></svg>
 
+                            <div class="text-xs">
+                                {{ \Carbon\Carbon::parse($ulasan->created_at)->isoformat('dddd, D-MMMM-YYYY') }}
+                            </div>
 
                         </div>
 
@@ -31,9 +33,7 @@
                         </div>
                         <p class="text-balance rounded-md py-2 px-2 text-sm">{{ $ulasan->ulasan }}</p>
                         <div class="flex items-center justify-end">
-                            <div class="text-xs">
-                                {{ \Carbon\Carbon::parse($ulasan->created_at)->isoformat('dddd, D-MMMM-YYYY') }}
-                            </div>
+
                         </div>
                     </div>
                 </div>
