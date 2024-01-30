78cfaf10dda43df29899e0f1e0e753412da8da1d
rendi <rendydwisaputra250@gmail.com>
2024-01-30 11:30:01 +0700

    route admin

---
+++ app/Http/Controllers/PenarikanAdminController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Penarikan;
+use App\Models\Transaksi;
+use Illuminate\Support\Facades\Auth;
+
+class PenarikanAdminController extends Controller
+{
+    public function index()
+    {
+        $penarikanTransaksi = [];
+
+        foreach (Penarikan::where('user_id', Auth::user()->id)->get() as $penarikan) {
+            $penarikanTransaksi[] = [
+                'type' => 'penarikan',
+                'data' => $penarikan
+            ];
+        }
+
+        foreach (Transaksi::where('admin_id', Auth::user()->id)->get() as $transaksi) {
+            $penarikanTransaksi[] = [
+                'type' => 'transaksi',
+                'data' => $transaksi
+            ];
+        }
+        return view('admin.penarikan', compact('penarikanTransaksi'));
+    }
+
+    public function tambah(Request $request)
+    {
+        // dd($request->all());
+        $request->validate([
+            'nominal' => 'required',
+            'no_rek' => 'required',
+            'metode_pembayaran' => 'required|in:BNI,BRI,BCA',
+        ], [
+            'nominal.required' => 'Nominal wajib diisi.',
+            'no_rek.required' => 'Nomor rekening wajib diisi.',
+            'metode_pembayaran.required' => 'Metode pembayaran wajib diisi.',
+            'metode_pembayaran.in' => 'Metode pembayaran harus salah satu dari: BNI, BRI, BCA.',
+        ]);
+        try {
+            $userid = Auth::user();
+            $user = User::find($userid->id);
+
+            if ($user->pendapatan < $request->nominal) {
+                return redirect()->back()->with('error', 'Saldo anda tidak mencukupi');
+            }
+
+            $user->pendapatan -= $request->nominal;
+            $user->save();
+
+            $penarikan = Penarikan::create([
+                'user_id' => $user->id,
+                'nominal' => $request->nominal,
+                'no_rek' => $request->no_rek,
+                'metode_pembayaran' => $request->metode_pembayaran
+            ]);
+            return redirect()->back()->with('success', 'Penarikan Berhasil Di Tambah');
+        } catch (\Exception $e) {
+            //throw $th;
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
+}
---
+++ resources/views/admin/penarikan.blade.php
@@ -0,0 +1,124 @@
+@extends('layout.main')
+
+@extends('layout.sidebar')
+
+@section('isi')
+    <div class="sm:ml-64 px-10 py-20">
+        <div class="bg-[#4F6F52] py-5 px-20 w-full rounded-lg">
+            <div class="flex justify-between items-center">
+                <div class="informasi flex flex-col">
+                    <span class="text-xl text-gray-300 font-semibold">Total Saldo Anda</span>
+                    <span class="text-3xl text-gray-300 font-semibold ms-3 mt-3">Rp.
+                        {{ number_format(Auth::user()->pendapatan, 0, ',', '.') }}</span>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-wallet text-[100px] text-gray-100/50"></i>
+                </div>
+            </div>
+        </div>
+        <form action="{{ route('owner.penarikan.tambah') }}" method="POST" class="my-12">
+            @csrf
+            <div class="grid grid-cols-2 gap-8">
+                <div class="metode">
+                    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900">Metode Pembayaran</label>
+                    <select id="countries" name="metode_pembayaran"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                        <option selected disabled>Metode Pembayaran</option>
+                        <option value="BRI">Bank Bri</option>
+                        <option value="BNI">Bank Bni</option>
+                        <option value="BCA">Bank Bca</option>
+                    </select>
+                </div>
+                <div class="mb-6">
+                    <label for="norek" class="block mb-2 text-sm font-medium text-gray-900">No Rekening</label>
+                    <input placeholder="Cth: 123456789" name="no_rek" type="number" id="norek"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    @error('no_rek')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="mb-6">
+                <label for="nominal class=" class="block mb-2 text-sm font-medium text-gray-900">Nominal</label>
+                <input type="text" placeholder="Cth: 100000" id="nominal" name="nominal"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                @error('nominal')
+                    <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="w-full flex justify-center items-center">
+                <button type="submit" class="py-2 px-8 text-white text-lg font-semibold rounded-lg"
+                    style="background-color: #4F6F52;">Tarik</button>
+            </div>
+        </form>
+        <div class="history">
+            <div class="flex justify-between px-20">
+                <div class="title">
+                    <span class="text-xl text-[#4F6F52] ">Riwayat</span>
+                </div>
+                <div class="indikator flex items-center gap-5">
+                    <div class="masuk flex items-center gap-2">
+                        <div class="bg-[#739072] rounded-full w-3 h-3"></div>
+                        <span class="text-lg text-gray-900">Dana Masuk</span>
+                    </div>
+                    <div class="masuk flex items-center gap-2">
+                        <div class="bg-[#ce6262] rounded-full w-3 h-3"></div>
+                        <span class="text-lg text-gray-900">Dana Keluar</span>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-1 gap-5 my-10 px-24">
+                @php
+                    $sortedPenarikanTransaksi = collect($penarikanTransaksi)->sortBy('data.waktu');
+                @endphp
+                @foreach ($sortedPenarikanTransaksi as $item)
+                    @if ($item['type'] == 'transaksi')
+                        {{-- History masuk --}}
+                        <div class="grid grid-cols-2 items-center">
+                            <div class="informasinya flex items-center gap-5">
+                                <div class="bg-[#739072] rounded-full w-4 h-4"></div>
+                                <div class="text">
+                                    <span class="text-xl font-medium text-gray-900">Transfer dari
+                                        {{ $item['data']->user->name }}</span>
+                                    <div class="flex gap-8 items-center">
+                                        <span
+                                            class="text-sm text-gray-900">{{ $item['data']->created_at->diffForHumans() }}</span>
+                                        <span
+                                            class="text-xs text-gray-900">{{ $item['data']->created_at->isoFormat('dddd, D MMMM Y') }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-xl text-[#739072] text-end">
+                                + Rp. {{ number_format($item['data']->nominal_owner, 0, ',', '.') }}
+                            </div>
+                        </div>
+                        {{-- History masuk --}}
+                    @elseif($item['type'] == 'penarikan')
+                        {{-- History Tarik --}}
+                        <div class="grid grid-cols-2 items-center">
+                            <div class="informasinya flex items-center gap-5">
+                                <div class="bg-[#ce6262] rounded-full w-4 h-4"></div>
+                                <div class="text">
+                                    <span class="text-xl font-medium text-gray-900">Berhasil melakukan penarikan
+                                        tunai</span>
+                                    <div class="flex gap-8 items-center">
+                                        <span
+                                            class="text-sm text-gray-900">{{ $item['data']->created_at->diffForHumans() }}</span>
+                                        <span
+                                            class="text-xs text-gray-900">{{ $item['data']->created_at->isoFormat('dddd, D MMMM Y') }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-xl text-[#ce6262] text-end">
+                                - Rp. {{ number_format($item['data']->nominal, 0, ',', '.') }}
+                            </div>
+                        </div>
+                        {{-- History Tarik --}}
+                    @endif
+                @endforeach
+
+                {{-- perulangan --}}
+            </div>
+        </div>
+    </div>
+@endsection
