4728b8a6444b4a23b96ef3d706830736cde83476
Irsyadandhikaariadi <113318445+Irsyadandhikaariadi@users.noreply.github.com>
2024-01-30 09:20:30 +0700

    titik

--- resources/views/owner/withdraw.blade.php
+++ resources/views/owner/withdraw.blade.php
@@ -31,7 +31,7 @@
                 </div>
             </div>
             <div class="mb-6">
-                <label for="nominal class="block mb-2 text-sm font-medium text-gray-900">Nominal</label>
+                <label for="nominal class=" class="block mb-2 text-sm font-medium text-gray-900">Nominal</label>
                 <input type="text" id="nominal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
             </div>
             <div class="w-full flex justify-center items-center">
